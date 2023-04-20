e5fd7b18f3d310159b0ea8fbe87ef8247dc67ade
NolanNie <85528096+NolanNie@users.noreply.github.com>
2023-04-20 14:13:04 -0500

    basemap

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-//insert code here!
+var map;
+
+function createMap(){
+
+    //create the map
+    map = L.map('map', {
+        center: [44.5, -89],
+        zoom: 6.50
+    
+    });
+
+    //add StadiaMaps base tilelayer
+    L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+	    maxZoom: 20,
+	    attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    //call getData function
+};
+
+
+
+document.addEventListener('DOMContentLoaded',createMap);
