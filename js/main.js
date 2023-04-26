a8ca18feaa550fa6b8e1872dd5d289e6548c61dd
nsticha <nsticha@wisc.edu>
2023-04-26 12:28:40 -0500

    final

--- js/main.js
+++ js/main.js
@@ -17,6 +17,20 @@ function createMap(){
 
     //call getData function
 };
+function search_birds() {
+  let input = document.getElementById('searchbar').value
+  input=input.toLowerCase();
+  let x = document.getElementsByClassName('common_name');
+    
+  for (i = 0; i < x.length; i++) { 
+      if (!x[i].innerHTML.toLowerCase().includes(input)) {
+          x[i].style.display="none";
+      }
+      else {
+          x[i].style.display="list-item";                 
+      }
+  }
+}
 
 
 
